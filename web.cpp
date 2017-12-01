9464ae721942c12e409190c989fc17576beffc35
Eugene <eugene1998@sbcglobal.net>
2017-12-01 00:10:49 -0600

    so close

--- web.cpp
+++ web.cpp
@@ -13,8 +13,8 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
 	/*if (argc < 2) {
-	std::cerr << "Usage: spellchecker inputfile=input.txt" << std::endl;
-	return -1;
+		std::cerr << "Usage: spellchecker inputfile=input.txt" << std::endl;
+		return -1;
 	}
 	ArgumentManager am(argc, argv);
 	const std::string script = am.get("script");
@@ -48,95 +48,100 @@ int main(int argc, char* argv[]) {
 				filenames.insert(newline);
 				while (getline(thefiles, theline)) {
 					//theline is inside
-					
+
 					if (theline.substr(0, 7) == "<a href") {
-						
+
 						int file = theline.find_first_of("\"");
 						string c = theline.substr(file + 1);
 						int file2 = c.find_last_of("\"");
 						c.erase(file2, c.length());
 						//c is what it links to
 						V.insert(c);
-						
+
 					}
 					for (auto i = V.begin(); i != V.end(); i++) {
-						//cout << "pass" << endl;
-						//cout << *i << endl;
+
 					}
-					
+
 
 				}
 				E.insert(make_pair(newline, V));
-				//E.insert_or_assign(newline, V);
+
 				V.clear();
 
 			}
 
 		}
-		
-		
+
+
 
 	}
-	//cout << E.size();
+
 	int k = 0;
 	for (auto& x : E) {
-		//cout << x.first << ": ";
-		for (unordered_set<string>::iterator itr = x.second.begin(); itr != x.second.end();itr++) {
-			//cout << *itr << " ";
+		cout << x.first << ": ";
+		for (unordered_set<string>::iterator itr = x.second.begin(); itr != x.second.end(); itr++) {
+			cout << *itr << " ";
 			k++;
 		}
-		//cout << endl;
+		cout << endl;
 	}
 
 	unordered_map <string, int> indegrees;
 	for (auto&x : E) {
-		indegrees.insert(make_pair(x.first,0));
-		//indegrees.insert_or_assign(x.first,0);
-	}
-	for (auto&x : indegrees) {
-		//cout << x.first << " " << x.second << endl;
+		indegrees.insert(make_pair(x.first, 0));
+
 	}
 	for (auto& x : E) {
 		int indeg = 0;
-		//cout << x.first << ": ";
+
 		for (unordered_set<string>::iterator itr = x.second.begin(); itr != x.second.end(); itr++) {
-			//cout << *itr << " ";
 			indegrees.find(*itr)->second++;
 		}
-		//cout << endl;
+
 	}
 	for (auto&x : indegrees) {
 		//cout << x.first << " " << x.second << endl;
+	}
+	map<int, string> test;
+	for (auto&x : indegrees) {
+		cout << x.first << " " << x.second << endl;
+		test.insert(make_pair(x.second, x.first));
+	}
+	priority_queue<int> w;
+	for (auto&x : test) {
+		w.push(test.find);
 	}
 	//key = filename, value=indegree
 	priority_queue<int> q;
 	for (auto&x : indegrees) {
 		q.push(x.second);
 	}
-	
+	int current_indegree = q.top();
+	cout << "queue" << endl;
 	for (int i = 0; i < q.size(); i++) {
-		//cout << q.top();
-		q.pop();
+		//cout << q.top()<<endl;
+		//q.pop();
 	}
 
-	int current_indegree = q.top();
-	//cout << "INDEG" << current_indegree<<endl;
+	cout << q.size() << endl;
+	//cout << "indeg" << current_indegree << endl;
 	set<string> top3;
 	for (auto& x : indegrees) {
-		if (x.second == current_indegree) {
-			top3.insert(x.first);
-		}
 		if (x.second == 0) {
-			graph << "isolated=" << x.first<<endl;
+			graph << "isolated=" << x.first << endl;
 		}
+	}
+	if (top3.size() < 3) {
+		
 	}
 
-	graph << "m="<<k<<endl;
-	graph << "n=" << E.size()<<endl;
+	graph << "m=" << k << endl;
+	graph << "n=" << E.size() << endl;
 	for (string x : top3) {
-		graph << "top3="<<x << endl;
+		graph << "top3=" << x << endl;
 	}
-	
+
 	system("pause");
 	return 0;
 }
